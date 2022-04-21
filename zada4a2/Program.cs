1ed9d3f65e9421e150e61c583c8287565d7e9a90
Ruslan <ri4i23aminov@yandex.ru>
2022-04-22 01:37:30 +0300

    zada4a2

---
+++ zada4a2/Program.cs
@@ -0,0 +1,13 @@
+﻿// Напишите программу, которая принимает на вход число
+// и выдаёт сумму цифр в числе
+
+Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int result = 0;
+
+while(number > 0)
+{
+    result = result + number % 10;
+    number = number / 10;
+}
+Console.WriteLine(result);
